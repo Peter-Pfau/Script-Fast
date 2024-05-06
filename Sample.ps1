a4c233256ec9218ee81038612ac8bdaf1f245525
Peter-Pfau <peter.pfau@va.gov>
2024-05-05 20:28:20 -0500

    Initial commit

---
+++ Sample.ps1
@@ -0,0 +1,12 @@
+<#
+.NAME
+	Get-Sample.ps1
+.DESCRIPTION
+    List Lost and Found Container
+.EXAMPLE
+    'v17.med.va.gov' | .\Get-Sample.ps1
+.NOTES
+    Author: Peter PFau - OIT IO SS Directory Services <VHAETMADTeam@va.gov>
+    Date:   March 13, 2023   
+#>
+write-host "Get-Help Get-Sample.ps1 -Full for more information."
